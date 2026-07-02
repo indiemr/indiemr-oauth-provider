9bad384faaa971ae7b5b678403bbbf7bf3fe21d4
Senthil Athiban <senthilathiban0@gmail.com>
2026-07-02 20:57:16 +0530

    fix: serverlet_build_and_config_harcoded

--- api/src/main/java/org/openmrs/module/indiemroauthprovider/util/ModuleConfig.java
+++ api/src/main/java/org/openmrs/module/indiemroauthprovider/util/ModuleConfig.java
@@ -1,36 +1,41 @@
 package org.openmrs.module.indiemroauthprovider.util;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.stereotype.Component;
 
+/**
+ * Temporary hardcoded config for local testing. Replace with {@link ModuleConfigLoader} (env /
+ * application.yml) before production deploy.
+ */
 @Component("indiemroauthprovider.ModuleConfig")
 public class ModuleConfig {
 	
-	private ModuleConfigLoader loader;
+	private static final String PUBLIC_BASE_URL = "https://localhost";
 	
-	@PostConstruct
-	public void init() {
-		loader = new ModuleConfigLoader();
-	}
+	private static final String ENC_KEY = "908698183a7bd9a855cd7fa308b85e776387f447dda348cf8c9cb9ac83911e74";
+	
+	private static final String GOOGLE_CLIENT_ID = "711590727381-8phvuojcelets9lt0oml339j959nchph.apps.googleusercontent.com";
+	
+	private static final String GOOGLE_CLIENT_SECRET = "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";
+	
+	private static final String GOOGLE_REDIRECT_URI = "https://localhost/openmrs/ws/rest/v1/oauth/connect/callback";
 	
 	public String getPublicBaseUrl() {
-		return loader.getPublicBaseUrl();
+		return PUBLIC_BASE_URL;
 	}
 	
 	public String getEncKey() {
-		return loader.getEncKey();
+		return ENC_KEY;
 	}
 	
 	public String getGoogleClientId() {
-		return loader.getGoogleClientId();
+		return GOOGLE_CLIENT_ID;
 	}
 	
 	public String getGoogleClientSecret() {
-		return loader.getGoogleClientSecret();
+		return GOOGLE_CLIENT_SECRET;
 	}
 	
 	public String getGoogleRedirectUri() {
-		return loader.getGoogleRedirectUri();
+		return GOOGLE_REDIRECT_URI;
 	}
 }
